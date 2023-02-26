bab296995c6835b14b7ba5a68fb3037067334fc2
Johan Gawell <johan.bo.gawell@gmail.com>
2023-02-26 14:09:37 +0100

    Possible to move robots in UI

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -8,10 +8,11 @@ import com.soywiz.korim.color.*
 import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.vector.*
+import gamemodel.*
 
 suspend fun main() = Korge(width = 1024, height = 1024, bgcolor = Colors["#2b2b2b"]) {
-	val sceneContainer = sceneContainer()
-	sceneContainer.changeTo({ GameScene() })
+    val sceneContainer = sceneContainer()
+    sceneContainer.changeTo({ GameScene() })
 }
 
 
@@ -22,33 +23,52 @@ class GameScene : Scene() {
         val indent = 2
         val cellSize = (fieldSize - indent * 2) / 10.0
 
+        var gameModel = GameModel(listOf(Robot(Pos(4, 4), Direction.Right)))
+
         val bgField = roundRect(fieldSize, fieldSize, 5.0, fill = Colors["#b9aea0"]) {
             graphics {
                 it.position(indent, indent)
                 repeat(10) { x ->
                     repeat(10) { y ->
                         fill(Colors["#cec0b2"]) {
-                            roundRect(cellSize * x + indent, cellSize * y + indent, cellSize - indent * 2, cellSize - indent * 2, 5.0)
+                            roundRect(
+                                cellSize * x + indent,
+                                cellSize * y + indent,
+                                cellSize - indent * 2,
+                                cellSize - indent * 2,
+                                5.0
+                            )
                         }
                     }
                 }
             }
         }
 
-        var robotX = 4
-        var robotY = 6
-        val robot = image(resourcesVfs["robot2.png"].readBitmap()) {
-            position(indent + robotX * cellSize, indent + robotY * cellSize)
-            size(cellSize, cellSize)
-        }
+        val robots = gameModel.robots.map {
+            val robotView = image(resourcesVfs["robot2.png"].readBitmap()) {
+                position(indent + it.pos.x * cellSize, indent + it.pos.y * cellSize)
+                size(cellSize, cellSize)
+            }
+            it.id to robotView
+        }.toMap()
 
         keys {
             down {
-                when(it.key) {
-                    Key.LEFT -> robot.x -= cellSize
-                    Key.RIGHT -> robot.x += cellSize
-                    Key.UP -> robot.y -= cellSize
-                    Key.DOWN -> robot.y += cellSize
+                when (it.key) {
+                    Key.SPACE -> {
+                        val robotId = gameModel.robots.first().id
+                        when (val result =
+                            gameModel.controlRobot(robotId, ActionCard.MoveForward(2))) {
+                            is RobotActionResult.Moved -> {
+                                val viewRobot = robots.getValue(robotId)
+                                viewRobot.x = indent + result.newPosition.x * cellSize
+                                viewRobot.y = indent + result.newPosition.y * cellSize
+                                gameModel = result.gameModel
+                            }
+                        }
+
+                    }
+
                     else -> Unit
                 }
             }
