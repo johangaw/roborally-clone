3a55bf8f18725ec4d104c30579d99e7141e91cb3
Johan Gawell <johan.bo.gawell@gmail.com>
2023-03-29 14:51:45 +0200

    Add win condition check

---
+++ src/commonMain/kotlin/gamemodel/resolveWinner.kt
@@ -0,0 +1,12 @@
+package gamemodel
+
+fun GameModel.checkForWinner(): CheckForWinnerResult =
+    players.firstOrNull { player -> player.capturedCheckpoints.containsAll(this.checkpoints.map { it.id }) }?.let {
+        CheckForWinnerResult.WinnerFound(it.id)
+    } ?: CheckForWinnerResult.NoWinnerFound
+
+
+sealed class CheckForWinnerResult {
+    data class WinnerFound(val playerId: PlayerId): CheckForWinnerResult()
+    object NoWinnerFound: CheckForWinnerResult()
+}
