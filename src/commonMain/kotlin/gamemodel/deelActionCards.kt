7000321115586ca6b2aab17ac6d5cecf5c63e2d4
Johan Gawell <johan.bo.gawell@gmail.com>
2023-03-29 14:42:00 +0200

    Deal less cards to players with damaged robots

--- src/commonMain/kotlin/gamemodel/deelActionCards.kt
+++ src/commonMain/kotlin/gamemodel/deelActionCards.kt
@@ -1,26 +1,21 @@
 package gamemodel
 
 fun GameModel.dealActionCards(): DealActionCardResult {
-    val cardsPerPlayer = 11 // TODO this should change with the robots damage
-    val cardsToDeal = players.size * cardsPerPlayer
-
-    if (cardsToDeal > actionDrawPile.size) return shuffleActionCards().dealActionCards()
-
-    val hands = players.mapIndexed { index: Int, player: Player ->
-        player.id to actionDrawPile.subList(index * cardsPerPlayer, index * cardsPerPlayer + cardsPerPlayer)
-    }.toMap()
+    val availableCards = actionDrawPile + actionDiscardPile.shuffled()
+    val hands = players.runningFold(0) {start, player -> start + getRobot(player.id).health - 1 }
+        .zipWithNext()
+        .map { (from, to) -> availableCards.subList(from, to) }
+        .zip(players)
+        .associate { (cards, player) -> player.id to cards }
 
     return DealActionCardResult(
         copy(
             players = players.map { it.copy(hand = hands.getValue(it.id)) },
-            actionDrawPile = actionDrawPile.drop(hands.values.sumOf { it.size }),
-            actionDiscardPile = actionDiscardPile + actionDrawPile.take(hands.values.sumOf { it.size })
+            actionDrawPile = availableCards.drop(hands.values.sumOf { it.size }),
+            actionDiscardPile = emptyList()
         ),
         hands
     )
 }
-
-private fun GameModel.shuffleActionCards() =
-    copy(actionDrawPile = actionDiscardPile + actionDiscardPile.shuffled(), actionDiscardPile = emptyList())
 
 data class DealActionCardResult(val gameModel: GameModel, val hands: Map<PlayerId, List<ActionCard>>)
---
+++ src/commonTest/kotlin/gamemodel/DealActionCardTest.kt
@@ -0,0 +1,49 @@
+package gamemodel
+
+import org.junit.Test
+import kotlin.test.*
+
+class DealActionCardTest {
+
+    @Test
+    fun `when all robots have all their health left, it deals 9 cards to each`() {
+        val model = gameModel(
+            """
+            +|+|+|+|+|+|+
+            +     ↓ ↓   +
+        """.trimIndent()
+        )
+        val originalDrawPile = model.actionDrawPile
+
+        val result = model.dealActionCards()
+
+        val (p1, p2) = result.gameModel.players
+        assertEquals(9, p1.hand.size)
+        assertEquals(9, p2.hand.size)
+        assertEquals(originalDrawPile.size - 9 - 9, result.gameModel.actionDrawPile.size)
+        assertEquals(0, result.gameModel.actionDiscardPile.size)
+    }
+
+    @Test
+    fun `when a robot has 9 health, it receives 8 cards`() = hasReceivedCorrectHand(9, 8)
+
+    @Test
+    fun `when a robot has 5 health, it receives 4 cards`() = hasReceivedCorrectHand(5, 4)
+
+    @Test
+    fun `when a robot has 1 health, it receives 0 cards`() = hasReceivedCorrectHand(9, 8)
+
+    private fun hasReceivedCorrectHand(health: Int, expectedHandSize: Int) {
+        val model = gameModel(
+            """
+            +|+|+|+|+|+|+
+            +     ↓     +
+        """.trimIndent()
+        ).let { model -> model.copy(robots = model.robots.map { it.copy(health = health) }) }
+
+        val result = model.dealActionCards()
+
+        assertEquals(expectedHandSize, result.gameModel.players.first().hand.size)
+        assertEquals(expectedHandSize, result.hands.values.first().size)
+    }
+}
